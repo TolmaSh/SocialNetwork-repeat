0827a2462c91a045383a7804fa082a48fa3560cf
TolmaSh <m0v4anua@gmail.com>
2022-10-24 03:05:06 +0300

    feat: added components navbar,header,profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import {Header} from './components/header/Header';
+import {Navbar} from './components/navbar/Navbar';
+import {Profile} from './components/profile/Profile';
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <Header/>
+        <div>
+            <Navbar/>
+            <Profile/>
+        </div>
     </div>
   );
 }

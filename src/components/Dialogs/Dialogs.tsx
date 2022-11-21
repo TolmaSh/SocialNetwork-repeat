ad2113c1fc12e6eafb5eb0ef45d15f9db6a9d8e3
TolmaSh <m0v4anua@gmail.com>
2022-11-21 23:08:45 +0200

    feat: dialogsData

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,14 +3,22 @@ import s from './Dialogs.module.scss'
 import {Message} from './Message/Message';
 import {User} from './User/User';
 
-export const Dialogs = () => {
+const dialogsData = [
+    {id:1, name: 'Tim'},
+    {id:2, name: 'Bob'},
+    {id:3, name: 'John'},
+    {id:4, name: 'Olga'},
+    {id:5, name: 'Horton'},
+    {id:6, name: 'Harry'},
+]
+
+export const Dialogs = ()  => {
     return (
         <div className={s.dialogs}>
             <ul className={s.users}>
-                <User user={'Tim'}/>
-                <User user={'David'}/>
-                <User user={'Tom'}/>
-                <User user={'Hardin'}/>
+                {dialogsData.map(u => {
+                    return <User key={u.id} user={u.name}/>
+                })}
             </ul>
             <ul className={s.messages}>
                 <Message message={'Hi'}/>

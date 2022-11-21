6b645050dddc62228e913f9638653dd1441c258b
TolmaSh <m0v4anua@gmail.com>
2022-11-21 23:27:32 +0200

    feat: postsData

--- src/components/profile/myposts/myposts.tsx
+++ src/components/profile/myposts/myposts.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {Post} from './post';
 
 export const Myposts = () => {
+    const postsData = [
+        {id: 1, post: 'Hi'},
+        {id: 2, post: 'I see you'},
+        {id: 3, post: 'I want drink beer'},
+        {id: 4, post: 'How are you?'},
+    ]
+
+    const mappedPosts = postsData.map(p=><Post postText={p.post}/>)
+
     return (
         <ul>
-            <Post postText={'Hi'} />
-            <Post postText={'HEllo'} />
-            <Post postText={'HEllo'} />
-            <Post postText={'HEllo'} />
+            {mappedPosts}
         </ul>
     );
 };

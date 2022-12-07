b31344930059d422b58920c7ed3d1c90eca4aaf4
TolmaSh <m0v4anua@gmail.com>
2022-12-07 13:25:30 +0200

    feat:[31] onClick, ref

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {RefObject} from 'react';
 import s from './Dialogs.module.scss'
 import {Message} from './Message/Message';
 import {User} from './User/User';
@@ -8,20 +8,34 @@ type PropsType = {
     state: DialogsPageType
 }
 
-export const Dialogs:React.FC<PropsType> = (props)  => {
-        const {dialogs,messages} = props.state
+export const Dialogs: React.FC<PropsType> = (props) => {
+    const {dialogs, messages} = props.state
+
+    const newPostText:any = React.createRef()
+    const addPostCallback = () => {
+        const text = newPostText.current.value
+        alert(text)
+    }
+
     return (
-        <div className={s.dialogs}>
-            <ul className={s.users}>
-                {dialogs.map(u => {
-                    return <User key={u.id} user={u.name}/>
-                })}
-            </ul>
-            <ul className={s.messages}>
-                {messages.map(m=> {
-                    return <Message key={m.id} message={m.message}/>
-                })}
-            </ul>
-        </div>
+            <div className={s.dialogs}>
+                <div className={s.dialogsWrapper}>
+                    <ul className={s.users}>
+                        {dialogs.map(u => {
+                            return <User key={u.id} user={u.name}/>
+                        })}
+                    </ul>
+                    <ul className={s.messages}>
+                        {messages.map(m => {
+                            return <Message key={m.id} message={m.message}/>
+                        })}
+                    </ul>
+                </div>
+
+                <textarea className={s.dialogsTextArea} ref={newPostText}></textarea>
+                <button className={s.dialogsButton} onClick={addPostCallback}>Add post</button>
+            </div>
+
+
     );
 };
